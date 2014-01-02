adf7f3d8bac431de40d2ad5efc300f23b1055d50
Atsuki Demizu <demmys.atsuki@gmail.com>
2014-01-02 17:31:04 +0900

    implemented: cursor blink of shell-view.js

--- js/code.js
+++ js/code.js
@@ -1,19 +1,17 @@
 $(function(){
-    var $shell = $('#shell');
-    $shell.typist({
-        height: $shell.height(),
-        width: $shell.width(),
+    var $hakoniwa = $("#hakoniwa");
+    window.controller = $hakoniwa.initShellView();
+
+    var $legacy = $('#legacy');
+    $legacy.typist({
+        height: $legacy.height(),
+        width: $legacy.width(),
         backgroundColor: '#fff',
         textColor: '#444',
         fontFamily: '"Droid Sans Mono", sans-serif;'
     }).typist('prompt');
 
-    demmy.code.run($shell, demmy.code.c);
-    window.setInterval(function(){
-        if($shell.queue().length == 0){
-            demmy.code.run($shell, demmy.code.c);
-        }
-    }, 2000);
+    demmy.code.random($legacy);
 });
 
 (function(window, library, namespace, undefined){
@@ -39,7 +37,9 @@ $(function(){
         return len;
     };
 
-    ns.c = {
+    var codes = new Array();
+
+    ns.c = codes.push({
         file: 'hello.c',
         source: [
             '#include <stdio.h>',
@@ -52,9 +52,33 @@ $(function(){
         compile: 'gcc -o hello hello.c',
         run: './hello',
         result: 'Hello, World!'
-    };
+    });
+
+    ns.java = codes.push({
+        before: function($shell){
+            $shell.typist('wait', 500)
+                  .typist('type', 'mkdir -p jp/demmy')
+                  .typist('prompt');
+        },
+        file: 'jp/demmy/Hello.java',
+        source: [
+            'package jp.demmy;',
+            ' ',
+            'class Hello{',
+            '    public static void main(String[] args){',
+            '        System.out.println("Hello, World!");',
+            '    }',
+            '}'
+        ],
+        compile: 'javac jp/demmy/Hello.java',
+        run: 'java jp/demmy/Hello',
+        result: 'Hello, World!'
+    });
 
     ns.run = function($shell, code){
+        if(typeof(code.before) == "function"){
+            code.before($shell);
+        }
         $shell.typist('wait', 500)
               .typist('type', 'ed -p @')
               .typist('wait', 1000)
@@ -70,15 +94,31 @@ $(function(){
               .typist('wait', 500)
               .typist('type', 'wq ' + code.file)
               .typist('echo', String(ns.length(code.source)))
-              .typist('prompt')
-              .typist('wait', 500)
-              .typist('type', code.compile)
-              .typist('wait', 500)
-              .typist('prompt')
-              .typist('wait', 500)
+              .typist('prompt');
+        if(typeof(code.compile) != "undefined"){
+            $shell.typist('wait', 500)
+                  .typist('type', code.compile)
+                  .typist('wait', 500)
+                  .typist('prompt');
+        }
+        $shell.typist('wait', 500)
               .typist('type', code.run)
               .typist('show', code.result)
               .typist('prompt');
+    };
+
+    ns.random = function($shell){
+        var r;
+        var show = function(){
+            r = Math.floor(Math.random() * codes.length);
+            ns.run($shell, codes[r]);
+        };
+        window.setInterval(function(){
+            var queue = $shell.queue();
+            if(typeof(queue) != "undefined" && queue.length == 0){
+                show();
+            }
+        }, 2000);
     };
 
 }(this, 'demmy', 'code'));
